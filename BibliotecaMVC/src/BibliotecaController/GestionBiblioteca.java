e82d267b4c6250aa0cb44b0f3875b93f5f5ec1ce
henryalmachi2491-cpu <henry.almachi2491@utc.edu.ec>
2026-06-19 09:47:35 -0500

    Snaider 8

--- BibliotecaMVC/src/BibliotecaController/GestionBiblioteca.java
+++ BibliotecaMVC/src/BibliotecaController/GestionBiblioteca.java
@@ -31,7 +31,7 @@ public class GestionBiblioteca {
         System.out.println("4");
         System.out.println("5");
         System.out.println("6");
-        System.out.println("7");
+        System.out.println("8");
     }
 
     public void actualizarLibro(String isbn, Libro libro) {
