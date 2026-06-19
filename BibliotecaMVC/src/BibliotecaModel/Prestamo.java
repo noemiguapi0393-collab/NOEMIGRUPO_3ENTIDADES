5f6fd1409a2944615d64f8b59ffdb14991585dfb
damaris17-q <anabel.lloacana8415@utc.edu.ec>
2026-06-18 19:44:45 -0500

    Estructura inicial de biblioteca con tres entidades

---
+++ BibliotecaMVC/src/BibliotecaModel/Prestamo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BibliotecaModel;
+
+/**
+ *
+ * @author DAMA
+ */
+public class Prestamo {
+    
+}
