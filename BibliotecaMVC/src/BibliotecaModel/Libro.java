54ce19c07539c505621cf3a801828fa358e5b1f1
michaeliza2057-dotcom <michael.iza2057@utc.edu.ec>
2026-06-19 04:18:45 -0500

    agregando contructor a la clase libro

--- BibliotecaMVC/src/BibliotecaModel/Libro.java
+++ BibliotecaMVC/src/BibliotecaModel/Libro.java
@@ -12,5 +12,9 @@ public class Libro {
     private String isbn;
     private String titulo;
     private String autor;
-    
+     public Libro(String isbn, String titulo, String autor) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+    }
 }
