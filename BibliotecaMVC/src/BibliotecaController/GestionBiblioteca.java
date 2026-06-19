167b6b76e90cf614963669fb7d5f00976adb0a38
henryalmachi2491-cpu <henry.almachi2491@utc.edu.ec>
2026-06-19 09:40:33 -0500

    commit 1
    Snaider

--- BibliotecaMVC/src/BibliotecaController/GestionBiblioteca.java
+++ BibliotecaMVC/src/BibliotecaController/GestionBiblioteca.java
@@ -10,41 +10,100 @@ import BibliotecaModel.Prestamo;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-
-/**
- *
- * @author DAMA
- */
 public class GestionBiblioteca {
-  private HashMap<String, Libro> mapaLibros;  
-  
-  private ArrayList<Usuario> listaUsuarios;
-  private ArrayList<Prestamo> listaPrestamos;
-  
-  public GestionBiblioteca(){
-      this.mapaLibros = new HashMap<>();
-      this.listaUsuarios = new ArrayList<>();
-      this.listaPrestamos = new ArrayList<>();
-  }
-  public void registrarLibro(String isbn, Libro libro){
-      mapaLibros.put(isbn, libro);
-      System.out.println("Libro guardado con exito");
-  }
-  public void registarUsuario(Usuario usuario){
-      listaUsuarios.add(usuario);
-      System.out.println("Usuario guardado con exito");
-  }
-  public void registrarPrestamo(Prestamo prestamo){
-      listaPrestamos.add(prestamo);
-      System.out.println("Prestamo registrado con exito");
-  }
-  public HashMap<String, Libro> getMapaLibros(){
-      return mapaLibros;
-  }   
-  public ArrayList<Usuario> getListaUsuarios(){
-      return listaUsuarios;
-  }
-  public ArrayList<Prestamo> getListaPrestamos(){
-      return listaPrestamos;
-  }
+
+    private HashMap<String, Libro> mapaLibros;
+    private ArrayList<Usuario> listaUsuarios;
+    private ArrayList<Prestamo> listaPrestamos;
+
+    public GestionBiblioteca() {
+        mapaLibros = new HashMap<>();
+        listaUsuarios = new ArrayList<>();
+        listaPrestamos = new ArrayList<>();
+    }
+
+    // LIBROS
+    public void registrarLibro(String isbn, Libro libro) {
+        mapaLibros.put(isbn, libro);
+        System.out.println("Libro guardado con éxito");
+    }
+
+    public void actualizarLibro(String isbn, Libro libro) {
+        if (mapaLibros.containsKey(isbn)) {
+            mapaLibros.put(isbn, libro);
+            System.out.println("Libro actualizado con éxito");
+        } else {
+            System.out.println("Libro no encontrado");
+        }
+    }
+
+    public void eliminarLibro(String isbn) {
+        if (mapaLibros.remove(isbn) != null) {
+            System.out.println("Libro eliminado con éxito");
+        } else {
+            System.out.println("Libro no encontrado");
+        }
+    }
+
+    // USUARIOS
+    public void registarUsuario(Usuario usuario) {
+        listaUsuarios.add(usuario);
+        System.out.println("Usuario guardado con éxito");
+    }
+
+    public void actualizarUsuario(int posicion, Usuario usuario) {
+        if (posicion >= 0 && posicion < listaUsuarios.size()) {
+            listaUsuarios.set(posicion, usuario);
+            System.out.println("Usuario actualizado con éxito");
+        } else {
+            System.out.println("Usuario no encontrado");
+        }
+    }
+
+    public void eliminarUsuario(int posicion) {
+        if (posicion >= 0 && posicion < listaUsuarios.size()) {
+            listaUsuarios.remove(posicion);
+            System.out.println("Usuario eliminado con éxito");
+        } else {
+            System.out.println("Usuario no encontrado");
+        }
+    }
+
+    // PRÉSTAMOS
+    public void registrarPrestamo(Prestamo prestamo) {
+        listaPrestamos.add(prestamo);
+        System.out.println("Préstamo registrado con éxito");
+    }
+
+    public void actualizarPrestamo(int posicion, Prestamo prestamo) {
+        if (posicion >= 0 && posicion < listaPrestamos.size()) {
+            listaPrestamos.set(posicion, prestamo);
+            System.out.println("Préstamo actualizado con éxito");
+        } else {
+            System.out.println("Préstamo no encontrado");
+        }
+    }
+
+    public void eliminarPrestamo(int posicion) {
+        if (posicion >= 0 && posicion < listaPrestamos.size()) {
+            listaPrestamos.remove(posicion);
+            System.out.println("Préstamo eliminado con éxito");
+        } else {
+            System.out.println("Préstamo no encontrado");
+        }
+    }
+
+    // GETTERS
+    public HashMap<String, Libro> getMapaLibros() {
+        return mapaLibros;
+    }
+
+    public ArrayList<Usuario> getListaUsuarios() {
+        return listaUsuarios;
+    }
+
+    public ArrayList<Prestamo> getListaPrestamos() {
+        return listaPrestamos;
+    }
 }
+
